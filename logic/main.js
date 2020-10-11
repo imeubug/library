9a0e601e2c0bf37fa63edf8cfd1492fb1d2f7642
imeubug <im.eubug@gmail.com>
2020-10-11 09:44:30 +0900

    Add a button to add and populate a new book

--- logic/main.js
+++ logic/main.js
@@ -5,36 +5,53 @@
 
 const bookshelf = document.getElementById('bookshelf');
 const newBookForm = document.getElementById('new-book-form');
+const addNewBookButton = document.getElementById('add-new-book');
 
 let myLibrary = [];
-addBook();
+// addBook();
 populate();
 
-function Book(title, author, pages, read) {
+function Book(title, author, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.read = read;
 
     this.info = function () {
         let read = (this.read) ? 'read' : 'not read yet';
-        return `${this.title} - ${this.author}. ${this.pages} pages. ${read}.`;
+        return `${this.title} - ${this.author}. ${read}.`;
     }
 }
 
+addNewBookButton.addEventListener('click', function(e) {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    let read = document.getElementsByName('read');
+    for(v in read) {
+        if (read[v].checked) {
+            read = read[v].value;
+            break;
+        }
+    }
+    console.log(read);
+    myLibrary.push([new Book(title, author, read), false]);
+    populate();
+    toggleForm();
+});
+
 function addBook() {
-    myLibrary.push(new Book('Stone of Ages 1', 'Jeon Min Hee', 500, 'read'));
-    myLibrary.push(new Book('Stone of Ages 2', 'Jeon Min Hee', 500, 'read'));
-    myLibrary.push(new Book('Stone of Ages 3', 'Jeon Min Hee', 500, 'read'));
-    myLibrary.push(new Book('Stone of Ages 4', 'Jeon Min Hee', 500, 'reading'));
-    myLibrary.push(new Book('책방', '저자', 500, 'not-yet'));
-    myLibrary.push(new Book('11の殺人', '東野圭吾', 500, 'not-yet'));
+    myLibrary.push([new Book('Stone of Ages 1', 'Jeon Min Hee', 500, 'read'), false]);
+    myLibrary.push([new Book('Stone of Ages 2', 'Jeon Min Hee', 500, 'read'), false]);
+    myLibrary.push([new Book('Stone of Ages 3', 'Jeon Min Hee', 500, 'read'), false]);
+    myLibrary.push([new Book('Stone of Ages 4', 'Jeon Min Hee', 500, 'reading'), false]);
+    myLibrary.push([new Book('title', 'author', 500, 'not-yet'), false]);
+    myLibrary.push([new Book('11の殺人', '東野 圭吾', 500, 'not-yet'), false]);
 }
 
 function populate() {
-    const newBookCard = bookshelf.firstElementChild;
+    // const newBookCard = bookshelf.firstElementChild;
 
     for (index in myLibrary) {
+        if(myLibrary[index][1]) continue;
         let card = document.createElement('div');
         card.className = 'card';
 
@@ -46,14 +63,15 @@ function populate() {
         let container = document.createElement('div');
         container.className = 'container';
         let book = myLibrary[index];
-        container.innerText = `${book.title} - ${book.author}`;
+        book[1] = true;
+        container.innerText = `${(book[0]).title} - ${(book[0]).author}`;
 
         card.appendChild(img);
         card.appendChild(container);
 
         bookshelf.appendChild(card);
     }
-    bookshelf.appendChild(newBookCard);
+    // bookshelf.appendChild(newBookCard);
 }
 
 function toggleForm() {
